ab89a5782209f2e7f4758dd4d46df8d018414c20
Geonosis Droid Factory <mobilecd@GeonosiMobileCD.office.gini.net>
2021-02-09 13:17:30 +0100

    Gini Capture SDK, Gini Capture Network Library Javadoc and Gini Capture Accounting Network Library Javadoc

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":ginicapture:dokkaHtml/androidTestRelease": [], ":ginicapture:dokkaHtml/debug": [], ":ginicapture:dokkaHtml/main": [], ":ginicapture:dokkaHtml/release": []}'
